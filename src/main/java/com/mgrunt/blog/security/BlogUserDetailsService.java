505feb8fab08856ce341b1f93442c989a5ca9165
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2025-08-07 21:12:10 +0200

    Implement authentication functionality with JWT support

---
+++ src/main/java/com/mgrunt/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.mgrunt.blog.security;
+
+import com.mgrunt.blog.domain.entities.User;
+import com.mgrunt.blog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email '" + email + "' not found."));
+
+        return new BlogUserDetails(user);
+    }
+}

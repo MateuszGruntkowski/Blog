2625f640f7738aee49c92312baea8e664f1ebc03
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2025-08-07 21:47:43 +0200

    Add create and delete functionality for tags with validation

--- src/main/java/com/mgrunt/blog/controllers/TagController.java
+++ src/main/java/com/mgrunt/blog/controllers/TagController.java
@@ -1,16 +1,17 @@
 package com.mgrunt.blog.controllers;
 
+import com.mgrunt.blog.domain.dtos.CreateTagsRequest;
 import com.mgrunt.blog.domain.dtos.TagResponse;
 import com.mgrunt.blog.domain.entities.Tag;
 import com.mgrunt.blog.mappers.TagMapper;
 import com.mgrunt.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tags")
@@ -25,5 +26,18 @@ public class TagController {
         List<Tag> tags = tagService.getTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest){
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(createdTagResponses, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/mgrunt/blog/services/TagService.java
+++ src/main/java/com/mgrunt/blog/services/TagService.java
@@ -3,7 +3,11 @@ package com.mgrunt.blog.services;
 import com.mgrunt.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
 }

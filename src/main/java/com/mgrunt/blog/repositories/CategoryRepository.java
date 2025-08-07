1630b8d0e08a9afe9e306d7ee2751286f29c5771
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2025-08-07 12:21:02 +0200

    add repositories for Category, Post, Tag, and User entities

---
+++ src/main/java/com/mgrunt/blog/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.mgrunt.blog.repositories;
+
+import com.mgrunt.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
